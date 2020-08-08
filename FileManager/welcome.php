e87a66d57926fda4813a658229858fa17ff8c7c2
AmiralSynux <61053212+AmiralSynux@users.noreply.github.com>
2020-08-08 20:00:09 +0300

    File

--- FileManager/welcome.php
+++ FileManager/welcome.php
@@ -3,6 +3,11 @@
 
 Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
+<?php
+    $file = fopen("data.txt","w");
+    fwrite($file,"I am the best");
+    fclose($file);
+?>
 
 </body>
 </html>
